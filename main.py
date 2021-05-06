00195c76a64313fb109565d4067eb3548d576ae9
akankaku1998 <>
2021-05-06 08:08:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import random
+def print_pattern(choice):
+  rock = '''
+      _______
+  ---'   ____)
+        (_____)
+        (_____)
+        (____)
+  ---.__(___)
+  '''
+
+  paper = '''
+      _______
+  ---'   ____)____
+            ______)
+            _______)
+          _______)
+  ---.__________)
+  '''
+
+  scissors = '''
+      _______
+  ---'   ____)____
+            ______)
+        __________)
+        (____)
+  ---.__(___)
+  '''
+  if choice==0:
+    print(rock)
+  elif choice == 1:
+    print(paper)
+  else:
+    print(scissors)
+  
+def result(n1,n2):
+  if n1==n2:
+    print("It's a draw")
+  elif n1 == 0:
+    if n2 == 1:
+      print("You lose")
+    else:
+      print("You win!")
+  elif n1 == 1:
+    if n2 == 2:
+      print("You lose")
+    else:
+      print("You win!")
+  elif n1 == 2:
+    if n2 == 0:
+      print("You lose")
+    else:
+      print("You win!")
+
+if __name__ == "__main__":
+  choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+  print_pattern(choice)
+  computer=random.randint(0,2)
+  print("Computer chose:")
+  print_pattern(computer)
+  result(choice,computer)
